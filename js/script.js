f70fdfd44b5ca6c90c6902befd41f9a400523f55
tamsay <longetemitope@gmail.com>
2020-06-18 20:19:42 +0100

    Desktop solution to the interest calculator app

---
+++ js/script.js
@@ -0,0 +1,74 @@
+let body = document.querySelector('body')
+let investmentPlans = document.querySelectorAll('option')
+let investmentGroup = document.querySelector('#plans')
+let interestBtn = document.querySelector('#calcInterestBtn')
+let rateDisplay = document.querySelector('#rateDisplay')
+let unitsInput = document.querySelector('#unitsDiv input')
+let time = document.querySelector('#monthsDiv input')
+let basePriceDisplay = document.querySelector('#investmentPlanDiv p')
+let baseDisplay = document.querySelector('#investmentPlanDiv p span')
+let interestSpan = document.querySelector('#interest')
+let totalAmountSpan = document.querySelector('#total')
+
+       
+investmentGroup.addEventListener('change', ()=>{
+    displayInterest()
+    basePriceDisplay.style.display = 'block'
+    baseDisplay.innerText = getBasePrice(getPlan());
+}) 
+
+let plansObj = [
+    {plan: '', interest: 0, basePrice: 0},
+    {plan: 'Maize', interest: 20, basePrice: 5000},
+    {plan: 'Transport', interest: 25, basePrice: 10000},
+    {plan: 'Estate', interest: 30, basePrice: 1500},
+    {plan: 'Uber', interest: 15, basePrice: 50000},
+]
+
+
+let displayInterest=()=>{
+    let plan = (investmentGroup.options[investmentGroup.selectedIndex].value);
+
+    plansObj.forEach(element=>{
+        if(plan.toLowerCase() === element.plan.toLowerCase()){
+            rateDisplay.innerText = element.interest
+        }
+    })
+}
+getPlan=()=>{
+    return investmentGroup.options[investmentGroup.selectedIndex].value;
+}
+getBasePrice=(plan)=>{
+    let price = 0
+    plansObj.forEach(element=>{
+        if(plan.toLowerCase() === element.plan.toLowerCase()){
+            price = (element.basePrice)
+        }
+    })
+    return price;
+}
+getInterest=(plan)=>{
+    let rate = 0
+    plansObj.forEach(element=>{
+        if(plan.toLowerCase() === element.plan.toLowerCase()){
+            rate = (element.interest)
+        }
+    })
+    return rate;
+}
+calculateInterest=(basePrice, rate, month, units)=>{
+    return interest = (((basePrice * rate * (month/12) * units)/100)).toFixed(2);
+}
+interestBtn.addEventListener('click', ()=>{
+    // investmentGroup.options[investmentGroup.selectedIndex].value
+    let basePrice = getBasePrice(getPlan());
+    let rate = getInterest(getPlan());
+    let month = Number(time.value);
+    let units = Number(unitsInput.value)
+    // console.log(units)
+    let total
+    let interest = calculateInterest(basePrice, rate, month, units);
+    interestSpan.innerText = interest;
+    totalAmountSpan.innerText = basePrice * units;
+})
+
